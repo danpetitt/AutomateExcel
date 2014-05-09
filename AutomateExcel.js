e8e7a2d14dd71953fda3d365cbb6a29914dd8337
Dan Petitt <danp@coderanger.com>
2014-05-09 17:32:44 +0100

    Added copyright notice

--- AutomateExcel.js
+++ AutomateExcel.js
@@ -1,78 +1,44 @@
-/*
-  A simple JavaScript object to automate Microsoft Excel from within Windows scripts
+/**
+ * AutomateExcel - A simple JavaScript object to automate Microsoft Excel from within Windows scripts
+ * @author Dan Petitt <danp@coderanger.com>
+ * Homepage: https://github.com/coderangerdan/AutomateExcel
+ *
+ * The MIT License (MIT)
+ * Copyright (c) 2014 Dan Petitt
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ */
 
-
-  Usage:
-    var excel = new Excel();
- 
-    excel.openFile( "d:\\testfile.xlsx" );
- 
-    // Find a named worksheet, delete the first row then save it out as UNICODE text
-    var sheetNum = excel.findWorkSheetNumber( "Sheet 2" );
-    if( sheetNum != -1 )
-    {
-      var ws = excel.getWorkSheet( sheetNum );
- 
-      // Delete a row range
-      var range = ws.Range( "A1", "AZ2" );
-      var row = range.EntireRow;
-      row.Delete();
- 
-      // Now save this worksheet as unicode text
-      var savedTextFile = excel.saveWorkSheet( sheetNum );
-    }
- 
-    excel.quit();
-
-
-    // Or you can do multiple files by re-using the Excel object which will be much quicker
-    var excel = new Excel();
- 
-    excel.openFile( "d:\\testfile.xlsx" );
-    excel.saveWorkSheet( 1, true );
-
-    excel.openFile( "d:\\testfile2.xlsx" );
-    excel.saveWorkSheet( 1, true );
-
-    excel.openFile( "d:\\testfile3.xlsx" );
-    excel.saveWorkSheet( 1, true );
-
-    excel.quit();
-
-
-    // You can supply a function for finding a worksheet name as the default just does a case insensitive comparison
-    var excel = new Excel();
- 
-    excel.openFile( "d:\\testfile.xlsx" );
- 
-    // Find a worksheet which contains specific text, then save it out as ANSI text
-    var sheetNum = excel.findWorkSheetNumber( function( workSheet ) {
-        var lookFor = "My Example Sheet Number ";
-        return workSheet.indexOf( lookFor.toLowerCase() ) != -1 ? true : false;
-      });
-    if( sheetNum != -1 )
-    {
-      excel.saveWorkSheet( sheetNum, false );
-    }
-
-    excel.quit();
-*/
- 
- 
 var Excel = function ()
 {
   var _filePath = "";
- 
+
   var _fso = new ActiveXObject( "scripting.filesystemobject" );
- 
+
   var _excel = new ActiveXObject( "Excel.Application" );
   var _wookbooks, _workbook;
- 
+
   var _xlTextWindows = 20;
   var _xlUnicodeText = 42;
   var _XlSaveAsAccessModexlNoChange = 1;
   var _xlDoNotSaveChanges = 2;
- 
+
   return {
     openFile: function ( file )
     {
@@ -90,12 +56,12 @@ var Excel = function ()
       _file = file;
 
       _excel.Visible = true;
- 
+
       _wookbooks = _excel.Workbooks;
- 
+
       _workbook = _wookbooks.Open( _filePath );
     },
- 
+
     findWorkSheetNumber: function ( findFunction )
     {
       if( typeof findFunction != "function" )
@@ -109,34 +75,34 @@ var Excel = function ()
       }
 
       var sheets = _workbook.Worksheets;
- 
+
       var total = sheets.Count;
       for ( var u = 1; u <= total ; u++ )
       {
         var ws = sheets.Item( u );
- 
+
         ws.Activate();
- 
+
         var worksheetName = ws.Name;
 
         if( findFunction( worksheetName ) )
           return u;
       }
- 
+
       // Not found
       return -1;
     },
- 
+
     getWorkSheet: function ( sheetNum )
     {
       var sheets = _workbook.Worksheets;
- 
+
       var ws = sheets.Item( sheetNum );
       ws.Activate();
- 
+
       return ws;
     },
- 
+
     trimColumnData: function( str )
     {
       str = str.trim();
@@ -144,10 +110,10 @@ var Excel = function ()
       {
         str = str.substring( 1, str.length - 2 ).trim();
       }
- 
+
       return str;
     },
- 
+
     saveWorkSheet: function ( sheetNum, asUnicode )
     {
       if( typeof asUnicode == "undefined" )
@@ -156,30 +122,30 @@ var Excel = function ()
       }
 
       var sheets = _workbook.Worksheets;
- 
+
       var ws = sheets.Item( sheetNum );
       ws.Activate();
- 
+
       var worksheetName = ws.Name;
- 
+
       var saveName = _filePath + "-" + sheetNum + "-(" + worksheetName + ").txt";
       saveName = saveName.replace( /\?/g, "" );
- 
+
       // Delete old file
       if ( _fso.fileExists( saveName ) )
       {
         _fso.DeleteFile( saveName );
       }
- 
+
       _workbook.SaveAs( saveName, asUnicode ? _xlUnicodeText : _xlTextWindows, "", "", false, false, _XlSaveAsAccessModexlNoChange );
- 
+
       return saveName;
     },
- 
+
     saveAllWorkSheets: function ()
     {
       var sheets = _workbook.Worksheets;
- 
+
       var total = sheets.Count;
       var sheets = [];
       for ( var u = 1; u <= total; u++ )
@@ -188,7 +154,7 @@ var Excel = function ()
       }
       return sheets;
     },
- 
+
     closeWorkbook: function ()
     {
       if( _workbook ) _workbook.Close( false );
@@ -200,7 +166,7 @@ var Excel = function ()
     quit: function ()
     {
       this.closeWorkbook();
- 
+
       if( _excel ) _excel.Quit();
 
       _excel = null;
